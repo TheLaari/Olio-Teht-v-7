88e01b72b6b9164f61f3fd1611c83e6b50a875e8
h8324 <h8324@OHJE-322-WS-13>
2017-02-01 18:30:13 +0200

    Initial commit.

---
+++ Olio-Teht-v-7/Olio-Teht-v-7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna vuosi > ");
+            string rivi = Console.ReadLine();
+            int vuosi;
+            bool result = Int32.TryParse(rivi, out vuosi);
+            if (result)
+            {
+                if ((vuosi % 4 == 0 && vuosi % 100 != 0) || vuosi % 400 == 0)
+                {
+                    Console.WriteLine("Vuosi on karkausvuosi.");
+                }
+                else
+                {
+                    Console.WriteLine("Vuosi ei ole karkausvuosi.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Et antanut numeroa!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
